798d0f5fef9ccf98f6aacd73aa9d1a0b175adf2f
akatonbo <akatonbobook@gmail.com>
2023-01-28 05:16:43 +0000

    attachments

--- src/commands/registerBotMsg.ts
+++ src/commands/registerBotMsg.ts
@@ -1,9 +1,112 @@
-import { CommandInteractionOptionResolver, SlashCommandAttachmentOption, SlashCommandBuilder } from 'discord.js';
+import { Attachment, CommandInteractionOptionResolver, SlashCommandBuilder, User } from 'discord.js';
 import { Command } from '../core';
-import fs from 'fs';
-import http from 'http';
+import { createWriteStream, unlink } from 'fs';
+import https from 'https';
 
-import { delBotMsg, getBotMsg, KeyAlreadyExistError, registerBotMsg } from '../botmessage/botmessage';
+import { BotMessage, sequelize } from '../db';
+import { KeyAlreadyExistError } from '../botmessage/botmessage';
+import path from 'path';
+
+type Download = (url: string, filename: string) => Promise<void>;
+type AddBotMsg = (key: string, content: string|null, attachment: Attachment|null, creator: User) => Promise<void>;
+type DelBotMsg = (key: string, user: User) => Promise<boolean>;
+type GetBotMsg = (user: User|null) => Promise<Array<BotMessage>>;
+
+const download: Download = async (url, filename) => {
+  return new Promise<void>((resolve, reject) => {
+    https
+      .request(url, (res) => {
+        res
+          .pipe(createWriteStream(filename))
+          .on("close", resolve)
+          .on("error", reject);
+      })
+      .end();
+  });
+};
+
+const addBotMsg: AddBotMsg = async (key, content, attachment, creator) => {
+  await sequelize.transaction(async t => {
+    const [botmessage, _isNew] = await BotMessage.findOrCreate({
+      where: {
+        key: key,
+      },
+      defaults: {
+        key: key,
+        content: content,
+        creatorId: creator.id,
+      },
+      transaction: t,
+      lock: t.LOCK.NO_KEY_UPDATE
+    });
+    if (!_isNew) {
+      throw new KeyAlreadyExistError(key);
+    }
+    if (attachment) {
+      let extension = "";
+      if (attachment.contentType == "image/png") {
+        extension = "png";
+      } else if (attachment.contentType == "image/jpeg") {
+        extension = "jpg";
+      } else {
+        throw new TypeError();
+      }
+      await download(attachment.url, path.join("..", "attachments", attachment.id + "." + extension));
+      await botmessage.update({
+        key: key,
+        url: attachment.id + "." + extension,
+        enable: true, 
+      }, { transaction: t });
+      console.log(`registered ${key}`);
+    }
+  });
+} 
+
+const delBotMsg: DelBotMsg = async (key, user) => {
+  let num = 0;
+  await sequelize.transaction(async t => {
+    const botmessages = await BotMessage.findAll({
+      where: {
+        key: key,
+        creatorId: user.id,
+      },
+      transaction: t,
+      lock: t.LOCK.UPDATE,
+    });
+    for(const botmessage of botmessages) {
+      if (botmessage.url) {
+        const filepath = path.join("..", "attachments", botmessage.url);
+        unlink(filepath, err => {
+          if (err) throw err;
+          console.log(`deleted ${filepath}`); 
+        });
+      }
+      await botmessage.destroy({ transaction: t });
+    }
+  });
+  return num > 0;
+};
+
+export const getBotMsg: GetBotMsg = async (user = null) => {
+  if (!!user) {
+    return await sequelize.transaction(async t => {
+      return await BotMessage.findAll({
+        where: {
+          creatorId: user.id,
+        },
+        transaction: t,
+        lock: t.LOCK.KEY_SHARE
+      });
+    });
+  } else {
+    return await sequelize.transaction(async t => {
+      return await BotMessage.findAll({
+        transaction: t,
+        lock: t.LOCK.KEY_SHARE
+      })
+    });
+  }
+}
 
 export const command = new Command(
   new SlashCommandBuilder()
@@ -49,25 +152,30 @@ export const command = new Command(
           const key = option.getString('key');
           const attachment = option.getAttachment('attachment');
           const content = option.getString('content');
+          await interaction.reply({content: `${key}を追加しています`, ephemeral: true});
           if (!key) {
             //  えらー
-            await interaction.reply({ content: "keyが必要です", ephemeral: true });
+            await interaction.editReply({ content: "keyが必要です"});
             return;
           }
           if (!attachment && !content) {
             //  えらー
-            await interaction.reply({ content: "attachmentまたはcontentが必要です", ephemeral: true });
+            await interaction.editReply({ content: "attachmentまたはcontentが必要です"});
             return;
           }
           try {
-            await registerBotMsg(key, content, attachment, interaction.user);
+            await addBotMsg(key, content, attachment, interaction.user);
           } catch (err) {
             if (err instanceof KeyAlreadyExistError) {
-              await interaction.reply(`The key(${key}) already exists.`);
+              await interaction.editReply(`すでにキーメッセージ:"${key}"が存在します`);
+              return;
+            } else {
+              console.error(err);
+              await interaction.editReply('Cant Registered');
               return;
             }
           }
-          await interaction.reply({ content: `Registered ${key}.`, ephemeral: true });
+          await interaction.followUp({ content: `新しいキーメッセージ:"${key}"を登録しました` });
           break;
         }
       case 'del':
@@ -79,7 +187,7 @@ export const command = new Command(
           }
           try {
             const deled = await delBotMsg(key, interaction.user);
-            if (deled) await interaction.reply({ content: `Deleted "${key}".`, ephemeral: true });
+            if (deled) await interaction.reply({ content: `キーメッセージ:"${key}"を削除しました.`, ephemeral: true });
             else await interaction.reply({ content: `"${key}" does not exist.\n※You can not delete a Message registered by others.`, ephemeral: true });
           } catch (err) {
             await interaction.reply({ content: "error", ephemeral: true });
@@ -93,7 +201,7 @@ export const command = new Command(
             const botMsgs = await getBotMsg(creator);
             let reply = "";
             if (botMsgs.length == 0) {
-              reply = "There are no Registered Message!";
+              reply = "登録されているキーメッセージはありません.";
             } else {
               let num = 0;
               for (const botMsg of botMsgs) {
--- src/events/message.ts
+++ src/events/message.ts
@@ -1,6 +1,30 @@
-import { Message } from "discord.js";
-import { getMsg } from "../botmessage/botmessage";
+import { BaseMessageOptions, Message } from "discord.js";
+import path from "path";
+import { KeyNotExitError } from "../botmessage/botmessage";
+
 import { EventListener } from "../core";
+import { BotMessage, sequelize } from "../db";
+
+type GetMsg = (key: string) => Promise<BaseMessageOptions|null>;
+
+const getMsg: GetMsg = async (key: string) => {
+  let bmo: BaseMessageOptions|null = null;
+  await sequelize.transaction(async t => {
+    const one = await BotMessage.findOne({
+      where: {
+        key: key
+      },
+      transaction: t,
+      lock: t.LOCK.KEY_SHARE
+    });
+    if (!one) throw new KeyNotExitError(key);
+    bmo = {
+      content: !!one.content ? one.content : undefined,
+      files: !!one.url ? [path.join("..", 'attachments', one.url)] : undefined,
+    };
+  });
+  return bmo;
+};
 
 export const listener = new EventListener(
   "messageCreate",
